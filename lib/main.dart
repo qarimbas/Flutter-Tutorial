003ac7bf7b5a139185ba0f834c1e2fdf1feb6f3b
s21kkurt <kkurt@sabanciuniv.edu>
2021-03-23 23:49:38 +0300

    Third Commit #10

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,11 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        child: Text(
-          "Hello People!",
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
-        ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
+        margin: EdgeInsets.all(30.0),
+        color: Colors.grey[400],
+        child: Text("Hello"),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
